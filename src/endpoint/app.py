4672625b90eb86c93f35a710b2912db36beac531
jggomez <juan.gomez01@gmail.com>
2024-04-02 22:44:58 -0600

    Fix CORS

--- src/endpoint/app.py
+++ src/endpoint/app.py
@@ -2,11 +2,19 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from . import questions_endpoint
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 
 def create_app():
     app = FastAPI()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(questions_endpoint.router)
     return app
